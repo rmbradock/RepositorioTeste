808dc2786ebcfa37874d0f17f32accc666d8a8f8
RICARDO BATISTA <batista.4995@aluno.pr.senac.br>
2023-09-11 11:10:21 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/repositorioteste/RepositorioTeste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositorioteste;
+
+/**
+ *
+ * @author batista.4995
+ */
+public class RepositorioTeste {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
